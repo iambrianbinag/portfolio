2b93ff0eb22f6ce6bc23ec7e9c8ac144e80f18b3
MutedFaith <brianbaenag@gmail.com>
2020-08-20 18:25:00 +0800

    Removed row

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { MDBRow, MDBCol } from "mdbreact";
-
 import html5 from "../img/logo-html5.png";
 import css3 from "../img/logo-css3.png";
 import javascript from "../img/logo-javascript.png";
@@ -26,115 +24,105 @@ import ScrollAnimation from "react-animate-on-scroll";
 const Skills = () => {
   return (
     <div
-      className="elegant-color-dark white-text text-center pt-5 pb-5"
+      className="elegant-color-dark white-text text-center pl-1 pr-1 pt-5 pb-5"
       id="skills"
     >
-      <MDBRow>
-        <MDBCol sm="12" md="10" className="offset-md-1">
-          <ScrollAnimation animateIn="fadeIn" animateOnce>
-            <h2 className="mt-3 mb-4">
-              <strong>SKILLS</strong>
-            </h2>
-            <div className="div-logo-group d-flex flex-wrap justify-content-center grey-text mb-3">
-              <div className="div-category-frontend bg-white m-1 p-1">
-                <div>Frontend</div>
-              </div>
-              <div className="div-category-backend bg-white m-1 p-1">
-                <div>Backend</div>
-              </div>
-              <div className="div-category-tools bg-white m-1 p-1">
-                <div>Development Tools</div>
-              </div>
-              <div className="div-category-others bg-white m-1 p-1">
-                <div>Others</div>
-              </div>
+      <div className="skills-wrapper">
+        <ScrollAnimation animateIn="fadeIn" animateOnce>
+          <h2 className="mt-3 mb-4">
+            <strong>SKILLS</strong>
+          </h2>
+          <div className="div-logo-group d-flex flex-wrap justify-content-center grey-text mb-3">
+            <div className="div-category-frontend bg-white m-1 p-1">
+              <div>Frontend</div>
             </div>
-            <hr className="bg-white" />
-            <div className="div-logo-group d-flex flex-wrap justify-content-center grey-text mt-4 pb-5">
-              <div className="div-category-frontend rounded bg-white m-1 p-1">
-                <img src={html5} alt="HTML" className="box-picture" />
-                <div className="logo-description">HTML</div>
-              </div>
-              <div className="div-category-frontend rounded bg-white m-1 p-1">
-                <img src={css3} alt="CSS" className="box-picture" />
-                <div className="logo-description">CSS3</div>
-              </div>
-              <div className="div-category-frontend rounded bg-white m-1 p-1">
-                <img
-                  src={javascript}
-                  alt="Javascript"
-                  className="box-picture"
-                />
-                <div className="logo-description">Javascript</div>
-              </div>
-              <div className="div-category-frontend rounded bg-white m-1 p-1">
-                <img src={bootstrap} alt="Bootstrap" className="box-picture" />
-                <div className="logo-description">Bootstrap</div>
-              </div>
-              <div className="div-category-frontend rounded bg-white m-1 p-1">
-                <img src={jquery} alt="jquery" className="box-picture" />
-                <div className="logo-description">JQuery</div>
-              </div>
-              <div className="div-category-frontend rounded bg-white m-1 p-1">
-                <img src={react} alt="React" className="box-picture" />
-                <div className="logo-description">React</div>
-              </div>
-              <div className="div-category-frontend rounded bg-white m-1 p-1">
-                <img src={materialUI} alt="React" className="box-picture" />
-                <div className="logo-description">Material UI</div>
-              </div>
-              <div className="div-category-backend rounded bg-white m-1 p-1">
-                <img src={php} alt="PHP" className="box-picture" />
-                <div className="logo-description">PHP</div>
-              </div>
-              <div className="div-category-backend rounded bg-white m-1 p-1">
-                <img src={laravel} alt="Laravel" className="box-picture" />
-                <div className="logo-description">Laravel</div>
-              </div>
-              <div className="div-category-backend rounded bg-white m-1 p-1">
-                <img src={rest} alt="Laravel" className="box-picture" />
-                <div className="logo-description">REST API</div>
-              </div>
-              <div className="div-category-backend rounded bg-white m-1 p-1">
-                <img src={mysql} alt="MySQL" className="box-picture" />
-                <div className="logo-description">MySQL</div>
-              </div>
-              <div className="div-category-others rounded bg-white m-1 p-1">
-                <img src={csharp} alt="C#" className="box-picture" />
-                <div className="logo-description">C#</div>
-              </div>
-              <div className="div-category-others rounded bg-white m-1 p-1">
-                <img src={java} alt="Java" className="box-picture" />
-                <div className="logo-description">Java</div>
-              </div>
-              <div className="div-category-tools rounded bg-white m-1 p-1">
-                <img src={git} alt="Java" className="box-picture" />
-                <div className="logo-description">Git</div>
-              </div>
-              <div className="div-category-tools rounded bg-white m-1 p-1">
-                <img src={postman} alt="Java" className="box-picture" />
-                <div className="logo-description">Postman</div>
-              </div>
-              <div className="div-category-tools rounded bg-white m-1 p-1">
-                <img src={visualStudio} alt="Java" className="box-picture" />
-                <div className="logo-description">VS</div>
-              </div>
-              <div className="div-category-tools rounded bg-white m-1 p-1">
-                <img
-                  src={visualStudioCode}
-                  alt="Java"
-                  className="box-picture"
-                />
-                <div className="logo-description">VS Code</div>
-              </div>
-              <div className="div-category-tools rounded bg-white m-1 p-1">
-                <img src={sublimeText} alt="Java" className="box-picture" />
-                <div className="logo-description">Sublime Text</div>
-              </div>
+            <div className="div-category-backend bg-white m-1 p-1">
+              <div>Backend</div>
             </div>
-          </ScrollAnimation>
-        </MDBCol>
-      </MDBRow>
+            <div className="div-category-tools bg-white m-1 p-1">
+              <div>Development Tools</div>
+            </div>
+            <div className="div-category-others bg-white m-1 p-1">
+              <div>Others</div>
+            </div>
+          </div>
+          <hr className="bg-white" />
+          <div className="div-logo-group d-flex flex-wrap justify-content-center grey-text mt-4 pb-5">
+            <div className="div-category-frontend rounded bg-white m-1 p-1">
+              <img src={html5} alt="HTML" className="box-picture" />
+              <div className="logo-description">HTML</div>
+            </div>
+            <div className="div-category-frontend rounded bg-white m-1 p-1">
+              <img src={css3} alt="CSS" className="box-picture" />
+              <div className="logo-description">CSS3</div>
+            </div>
+            <div className="div-category-frontend rounded bg-white m-1 p-1">
+              <img src={javascript} alt="Javascript" className="box-picture" />
+              <div className="logo-description">Javascript</div>
+            </div>
+            <div className="div-category-frontend rounded bg-white m-1 p-1">
+              <img src={bootstrap} alt="Bootstrap" className="box-picture" />
+              <div className="logo-description">Bootstrap</div>
+            </div>
+            <div className="div-category-frontend rounded bg-white m-1 p-1">
+              <img src={jquery} alt="jquery" className="box-picture" />
+              <div className="logo-description">JQuery</div>
+            </div>
+            <div className="div-category-frontend rounded bg-white m-1 p-1">
+              <img src={react} alt="React" className="box-picture" />
+              <div className="logo-description">React</div>
+            </div>
+            <div className="div-category-frontend rounded bg-white m-1 p-1">
+              <img src={materialUI} alt="React" className="box-picture" />
+              <div className="logo-description">Material UI</div>
+            </div>
+            <div className="div-category-backend rounded bg-white m-1 p-1">
+              <img src={php} alt="PHP" className="box-picture" />
+              <div className="logo-description">PHP</div>
+            </div>
+            <div className="div-category-backend rounded bg-white m-1 p-1">
+              <img src={laravel} alt="Laravel" className="box-picture" />
+              <div className="logo-description">Laravel</div>
+            </div>
+            <div className="div-category-backend rounded bg-white m-1 p-1">
+              <img src={rest} alt="Laravel" className="box-picture" />
+              <div className="logo-description">REST API</div>
+            </div>
+            <div className="div-category-backend rounded bg-white m-1 p-1">
+              <img src={mysql} alt="MySQL" className="box-picture" />
+              <div className="logo-description">MySQL</div>
+            </div>
+            <div className="div-category-others rounded bg-white m-1 p-1">
+              <img src={csharp} alt="C#" className="box-picture" />
+              <div className="logo-description">C#</div>
+            </div>
+            <div className="div-category-others rounded bg-white m-1 p-1">
+              <img src={java} alt="Java" className="box-picture" />
+              <div className="logo-description">Java</div>
+            </div>
+            <div className="div-category-tools rounded bg-white m-1 p-1">
+              <img src={git} alt="Java" className="box-picture" />
+              <div className="logo-description">Git</div>
+            </div>
+            <div className="div-category-tools rounded bg-white m-1 p-1">
+              <img src={postman} alt="Java" className="box-picture" />
+              <div className="logo-description">Postman</div>
+            </div>
+            <div className="div-category-tools rounded bg-white m-1 p-1">
+              <img src={visualStudio} alt="Java" className="box-picture" />
+              <div className="logo-description">VS</div>
+            </div>
+            <div className="div-category-tools rounded bg-white m-1 p-1">
+              <img src={visualStudioCode} alt="Java" className="box-picture" />
+              <div className="logo-description">VS Code</div>
+            </div>
+            <div className="div-category-tools rounded bg-white m-1 p-1">
+              <img src={sublimeText} alt="Java" className="box-picture" />
+              <div className="logo-description">Sublime Text</div>
+            </div>
+          </div>
+        </ScrollAnimation>
+      </div>
     </div>
   );
 };
