3a2bb4caffc8cbb1a09857b520c5193534e0389a
MutedFaith <brianbaenag@gmail.com>
2020-08-20 12:29:03 +0800

    Changed objective text

--- src/components/myprofile.jsx
+++ src/components/myprofile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Image from "../img/my-picture.jpg";
+import MyImage from "../img/my-picture.jpg";
 import ScrollAnimation from "react-animate-on-scroll";
 
 const Myprofile = () => {
   return (
     <div
-      className="d-flex flex-column justify-content-center align-items-center text-center pt-5 pb-5"
+      className="d-flex flex-column justify-content-center align-items-center text-center pl-2 pr-2 pt-5 pb-5"
       id="profile"
     >
       <ScrollAnimation animateIn="fadeIn" animateOnce>
@@ -13,7 +13,7 @@ const Myprofile = () => {
           <strong> MY PROFILE</strong>
         </h2>
 
-        <img src={Image} alt="Me" className="rounded-circle img-portrait" />
+        <img src={MyImage} alt="Me" className="rounded-circle img-portrait" />
         <h3 className="mt-4">
           <strong>Brian Binag</strong>
         </h3>
@@ -23,8 +23,8 @@ const Myprofile = () => {
           <strong>Objective</strong>
         </h3>
         <p className="mt-2 p-1 w-50 blue-grey-text w-100">
-          A goal getter, as well as a self-starter, striving to learn and hone
-          my skills especially in web development
+          I'm a goal getter, as well as a self-starter, striving to learn and
+          hone my skills in different technology.
         </p>
       </ScrollAnimation>
       <ScrollAnimation animateIn="fadeIn" animateOnce>
