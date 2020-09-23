9c60689041c6f3ce4c82002119f96ed74d80ae6a
MutedFaith <brianbaenag@gmail.com>
2020-09-23 20:08:28 +0800

    Added CV

--- src/components/navbarBottom.jsx
+++ src/components/navbarBottom.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { MDBBtn, MDBIcon } from "mdbreact";
+import myCurriculumVitae from "../files/Binag_CV.pdf";
 
 const Footer = () => {
   return (
     <div className="fixed-bottom d-flex justify-content-center elegant-color">
-      <MDBBtn outline color="white" size="sm">
-        <MDBIcon icon="download" className="mr-1" /> Download My CV
-      </MDBBtn>
+      <a href={myCurriculumVitae} download>
+        <MDBBtn outline color="white" size="sm">
+          <MDBIcon icon="download" className="mr-1" /> Download My CV
+        </MDBBtn>
+      </a>
     </div>
   );
 };
