d59979d13907d988f6a64d60b1e5b31d9b733a9c
MutedFaith <brianbaenag@gmail.com>
2020-10-24 18:27:05 +0800

    changed CV to Resume

--- src/components/navbarBottom.jsx
+++ src/components/navbarBottom.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { MDBBtn, MDBIcon } from "mdbreact";
-import myCurriculumVitae from "../files/Binag_CV.pdf";
+import myResume from "../files/Binag_Resume.pdf";
 
 const Footer = () => {
   return (
     <div className="fixed-bottom d-flex justify-content-center elegant-color">
-      <a href={myCurriculumVitae} download>
+      <a href={myResume} download>
         <MDBBtn outline color="white" size="sm">
-          <MDBIcon icon="download" className="mr-1" /> Download My CV
+          <MDBIcon icon="download" className="mr-1" /> Download My Resume
         </MDBBtn>
       </a>
     </div>
