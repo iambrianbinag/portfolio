05afdcc8101fbaed7b9458da80b558046a0e041f
iambrianbinag <brianbaenag@gmail.com>
2021-11-17 12:57:30 +0800

    Change resume download form file to link

--- src/components/NavbarBottom.jsx
+++ src/components/NavbarBottom.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { MDBBtn, MDBIcon } from "mdbreact";
-import myResume from "../files/Binag_Resume.pdf";
+import { myResumeLink } from "../resources/files";
 
 const Footer = () => {
   return (
     <div className="fixed-bottom d-flex justify-content-center elegant-color">
-      <a href={myResume} download>
+      <a href={myResumeLink} target="_blank">
         <MDBBtn outline color="white" size="sm">
           <MDBIcon icon="download" className="mr-1" /> Download My Resume
         </MDBBtn>
---
+++ src/resources/files.js
@@ -0,0 +1,3 @@
+const myResumeLink = 'https://drive.google.com/file/d/1MhUhGtPLaEa0YX6OtxOdzuuKIQ_3w2wr/view?usp=sharing';
+
+export { myResumeLink };
