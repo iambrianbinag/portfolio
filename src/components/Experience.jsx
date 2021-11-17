8ebd50f072d0414149221e2d29d941dbde32ce3c
iambrianbinag <brianbaenag@gmail.com>
2021-11-17 11:33:21 +0800

    Update experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -29,12 +29,16 @@ const Experience = () => {
               </span>
               <div className="step-content z-depth-1 ml-xl-0 p-0 mt-2">
                 <div className="timeline-header elegant-color-dark white-text p-3">
-                  <h4>Web Developer</h4>
+                  <h4>Backend Developer</h4>
                   <h5 className="m-0">LayBare Waxing Philippines Inc.</h5>
                 </div>
                 <p className="text-muted mt-4 px-4"><i className="far fa-clock" aria-hidden="true"></i> November 2020 - Current</p>
                 <p className="mb-0 px-4 pb-4">
-                  I develop and maintain the internal and external systems of the company
+                  -  Developed and maintained the existing system of the company.
+                  <br />
+                  -  Refactored and optimized the curent codebase.
+                  <br />
+                  -  API development.
                   <br/><br/>
                   Tech stack: Vue and Laravel
                 </p>
@@ -56,7 +60,9 @@ const Experience = () => {
                 </div>
                 <p className="text-muted mt-4 px-4"><i className="far fa-clock" aria-hidden="true"></i> October 2020 - October 2020</p>
                 <p className="mb-0 px-4 pb-4">
-                  I developed a game web portal site for the company wherein they used to showcase their game products to the possible clients. I also onboarded in <a href="https://siliconvalleyhq.com/" alt="SilliconValleyHQ" target="_blank" className="timeline-description-link" rel="noopener noreferrer">SilliconValleyHQ</a> project where I did the minor integration of frontend and backend part.
+                  - Developed a game web portal site for the company.
+                  <br/>
+                  - Developed the minor integration of frontend and backend.
                   <br/><br/>
                   Tech stack: HTML, CSS, JavaScript, Bootstrap, jQuery, Vue, and Laravel
                 </p>
