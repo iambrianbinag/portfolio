94f5fc35d067d34894039ebcad3f28eeaccde50c
iambrianbinag <brianbaenag@gmail.com>
2023-07-25 13:55:22 +0800

    Updates

---
+++ precache-manifest.ed021b5ffc0a6a52dd40b99dd6a01695.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c0de108a39fc9936cba76133a964c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ff06fe24f5b9d6f6b55",
+    "url": "./static/css/2.084c21ec.chunk.css"
+  },
+  {
+    "revision": "9fd682aa551eef023a3e",
+    "url": "./static/css/main.6d1a623d.chunk.css"
+  },
+  {
+    "revision": "4ff06fe24f5b9d6f6b55",
+    "url": "./static/js/2.c7ae0ac7.chunk.js"
+  },
+  {
+    "revision": "4c9f6bf0b2443eda3f2900d75f3caae2",
+    "url": "./static/js/2.c7ae0ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd682aa551eef023a3e",
+    "url": "./static/js/main.f6d8e965.chunk.js"
+  },
+  {
+    "revision": "e2e372adc99f7092c75b",
+    "url": "./static/js/runtime-main.778ba779.js"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "./static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "./static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "./static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "./static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "./static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "./static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "./static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "./static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "./static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "./static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "./static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "./static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "./static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "./static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "./static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "./static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "./static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "./static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "70c22f8a1696a9f97b833009684bc40e",
+    "url": "./static/media/logo-bootstrap.70c22f8a.png"
+  },
+  {
+    "revision": "63212037aca72ee418adb90d70577de4",
+    "url": "./static/media/logo-jquery.63212037.png"
+  },
+  {
+    "revision": "fc93dda84c5734f3f68add3b16980c0f",
+    "url": "./static/media/logo-react.fc93dda8.png"
+  },
+  {
+    "revision": "5df6355bb325cb2c95e089169aa98fbf",
+    "url": "./static/media/my-picture.5df6355b.jpg"
+  },
+  {
+    "revision": "599b7d29a110961a85dd2b8d03fef401",
+    "url": "./static/media/project-clinicrecordmanagementsystem.599b7d29.png"
+  },
+  {
+    "revision": "b2258d1bd9f92885ec0ad6ea2bc50fa2",
+    "url": "./static/media/project-covid-19-tracker.b2258d1b.png"
+  },
+  {
+    "revision": "ab45d91424032fe2e5f854837ed6eac4",
+    "url": "./static/media/project-generalclinicmanagementsystem.ab45d914.png"
+  },
+  {
+    "revision": "43bfddc2df75bb67313eadf27e30238e",
+    "url": "./static/media/project-posinventorysystem.43bfddc2.png"
+  },
+  {
+    "revision": "2b9de0d324f1be887d7e2aadfce2b835",
+    "url": "./static/media/project-siliconvalleyhq.2b9de0d3.png"
+  }
+]);
